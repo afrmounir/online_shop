21dc4fb645a44b65bb7fcdfb4564624a8dd73e76
Mounir <afrmounir@gmail.com>
2022-08-16 15:21:00 +0200

    add dummies middleware for test

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+  console.log('In middleware');
+  next();
+});
+
+app.use((req, res, next)=>{
+  console.log('In another middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
