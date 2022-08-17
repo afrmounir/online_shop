933dc42d6564ecc71d66acd666355d1d24d2a8a5
Mounir <afrmounir@gmail.com>
2022-08-17 07:42:02 +0200

    add Parsing Incoming Requests
    - adding form
    - removing console log for tests purpose

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
+
 app.use('/add-product', (req, res, next)=>{
-  console.log('In another middleware');
-  res.send('<h1>The Add Product</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next)=>{
-  console.log('In another middleware');
   res.send('<h1>Express</h1>');
 });
 
