7eadc4d0d2ec4d39da6d6836d7fe0f3989c57301
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-10 15:30:04 +0200

    feat: add link to the right pre-populate edit page

--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,14 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/edit-product => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/edit-product');
 
 module.exports = router;
