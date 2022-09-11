b79f59513cbf4711bc9296a665f5513ed41e574b
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-11 16:32:54 +0200

    feat: connect to the SQL database & retrieve Data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const errorController = require('./controllers/error');
+const db = require('./utilities/database');
 
 const app = express();
 
@@ -11,6 +12,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then(result => {
+    console.log(result[0]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
