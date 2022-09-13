742b1af813952ebacc3cc778b4475ccf4540c1b4
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-13 18:49:13 +0200

    feat: fetch admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,11 +59,14 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Produits (Admin)',
-      path: '/admin/products'
-    });
-  });
+  Product
+    .findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Produits (Admin)',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
