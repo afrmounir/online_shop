67b9b09081824c0e8a1b88ec14f7470f7367e6cf
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-14 11:42:09 +0200

    feat: fetch related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,9 +30,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const productId = req.params.productId;
-  Product
-    .findByPk(productId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: productId } })
+    .then(products => {
+      const product = products[0]; // we get back a array even its a single product
       if (!product) {
         return res.redirect('/');
       }
@@ -79,8 +80,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product
-    .findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
