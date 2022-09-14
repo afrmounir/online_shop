2429b48fead0d2ffc4a6859f27d3037243cf727d
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-14 09:58:14 +0200

    feat: create a User model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = require('../utilities/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
