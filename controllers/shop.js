99f6c79d656593056f101d2de09588ab4b730ba7
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-15 08:09:55 +0200

    feat: add new products to the cart (postCart)

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,18 +42,37 @@ exports.getProduct = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, product => {
-    Cart.addProduct(productId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart; // make the cart available in the nested Product
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      cart.getProducts({ where: { id: productId } });
+    })
+    .then(products => {
+      let product
+      if (products) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        //...
+      }
+      return Product
+        .findByPk(productId)
+        .then(product => {
+          return fetchedCart.addProduct(product, { through: { quantity: newQuantity } }); // say to sequelize which model to use in in-between model and for that table what additional information to set the values in there
+        })
+        .catch(err => console.log(err));
+    })
+    .then(() => res.redirect('/cart'))
+    .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart.getProducts();
-    })
+    .then(cart => cart.getProducts())
     .then(products => {
       res.render('shop/cart', {
         pageTitle: 'Panier',
