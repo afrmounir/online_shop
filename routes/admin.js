eed33ac4309f36c06dceac17c89cee243bc80d4c
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-20 11:11:58 +0200

    feat: make edit & delete button available again

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.get('/add-product', adminController.getAddProduct);
 
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
 
