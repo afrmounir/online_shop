051fed61d7e345afd5b92c90def3028ac0b27603
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-23 09:39:06 +0200

    fix: replace res.setHeader by res.cookie
    res.setHeader is now limited to setting the basic cookie value.
    Use res.cookie() for added functionality.

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=True');
+  res.cookie('loggedIn', true);
   res.redirect('/');
 };
