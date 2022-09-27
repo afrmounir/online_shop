15c7ba228c25a113ec38fab714883e29da643a88
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-27 08:51:51 +0200

    feat: add authorization to manage product by user

--- controllers/admin.js
+++ controllers/admin.js
@@ -67,7 +67,7 @@ exports.postDeleteProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product
-    .find()
+    .find({ userId: req.user._id })
     .then(products => {
       res.render('admin/products', {
         prods: products,
