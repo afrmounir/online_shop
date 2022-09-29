658222df53fc1641d9793e61eb9d48938b80a33e
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-29 08:13:07 +0200

    fix: avoid infinite loop due to error handling

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+const { error } = require('console');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -40,6 +41,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -53,14 +60,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -71,7 +72,11 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((err, req, res, next) => {
-  res.redirect('500');
+  //res.redirect('500');
+  res.status(500).render('500', {
+    pageTitle: 'Erreur 500',
+    path: '/500'
+  });
 });
 
 mongoose
