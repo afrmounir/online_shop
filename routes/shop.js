7d0789079157c08c4e2e1f2e8e7314d618f26a76
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-09-30 08:40:09 +0200

    feat: download files with authentification

--- routes/shop.js
+++ routes/shop.js
@@ -23,6 +23,8 @@ router.get('/cart', isAuth, shopController.getCart);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
 // router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
